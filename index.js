ed1d9aa33e1ccf29cdef848f3e0338690d961ca7
prakhar <prakhar.chhipa@airtel.com>
2020-05-21 00:27:34 +0530

    header added

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+  })
   .post('/process_post', urlencodedParser, function (req, res) {
     let i = 0;
     let url = req.body.url;
